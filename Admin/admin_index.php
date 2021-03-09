ee35b4f6220876524ce6bac38e2314e7420a26bf
NOP <57258174+mplein2@users.noreply.github.com>
2021-03-09 16:15:47 +0200

    css

--- Admin/admin_index.php
+++ Admin/admin_index.php
@@ -9,9 +9,9 @@
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome Icons -->
-  <link rel="stylesheet" href="../css/all.min.css">
+  <link rel="stylesheet" href="../css/fontawesome-free/css/all.min.css">
   <!-- Theme style -->
-  <link rel="stylesheet" href="../css/css/adminlte.css">
+  <link rel="stylesheet" href="../css/adminlte.min.css">
 </head>
 <body class="hold-transition sidebar-mini">
 <div class="wrapper">
