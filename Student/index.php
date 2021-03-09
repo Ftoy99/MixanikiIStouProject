2b9e84887a341d59cb3bdb8d0e1010d698e31f77
CChristofi <chambakis.cc@gmail.com>
2021-03-09 19:42:17 +0200

    students

--- Student/index.php
+++ Student/index.php
@@ -1,107 +1,165 @@
-<?php
-session_start();
-
-
-
-?>
-
 <!DOCTYPE html>
+
 <html lang="en">
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>SRS - Login</title>
+  <title>SRS - Admin</title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="css/fontawesome-free/css/all.min.css">
-  <!-- icheck bootstrap -->
-  <link rel="stylesheet" href="css/icheck-bootstrap.min.css">
+  <!-- Font Awesome Icons -->
+  <link rel="stylesheet" href="../css/fontawesome-free/css/all.min.css">
+  <link rel="stylesheet" href="../css/font-awesome/css/font-awesome.min.css">
   <!-- Theme style -->
-  <link rel="stylesheet" href="css/adminlte.min.css">
+  <link rel="stylesheet" href="../css/adminlte.min.css">
 </head>
-<body class="hold-transition login-page">
-<div class="login-box">
-  <!-- /.login-logo -->
-  <div class="card card-outline card-primary">
-    <div class="card-header text-center">
-      <p class="h1"><b>Student Record System</p>
-    </div>
-    <div class="card-body">
-      <p class="login-box-msg">Sign in to start your session</p>
-      <!-- Error Handling if Info Is Wrong .  -->
-      <?php
-      if (isset($_SESSION['LoginError'])){
+<body class="hold-transition sidebar-mini">
+<div class="wrapper">
 
-        if($_SESSION['LoginError']=='1'){
-          echo '<p class="login-box-msg" style="color: red;">Password Entered Dosent Match.</p>';
-        }
+  <!-- Navbar -->
+  <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+      </li>
+      <li class="nav-item d-none d-sm-inline-block">
+        <a href="dashboard.php" class="nav-link">Home</a>
+      </li>
+      <li class="nav-item d-none d-sm-inline-block">
+        <a href="#" class="nav-link">Contact</a>
+      </li>
+    </ul>
 
-        if($_SESSION['LoginError']=='2'){
-          echo '<p class="login-box-msg" style="color: red;">Account with this email dosent exist.</p>';
-        }
-
-        unset($_SESSION['LoginError']);
-      }
-     
-      ?>
-      <form action="Php/login.php" method="post">
-        <div class="input-group mb-3">
-          <input type="email" class="form-control" placeholder="Email" name="email">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-envelope"></span>
-            </div>
-          </div>
+    <!-- SEARCH FORM -->
+    <form class="form-inline ml-3">
+      <div class="input-group input-group-sm">
+        <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
+        <div class="input-group-append">
+          <button class="btn btn-navbar" type="submit">
+            <i class="fas fa-search"></i>
+          </button>
         </div>
-        <div class="input-group mb-3">
-          <input type="password" class="form-control" placeholder="Password" name="password">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-lock"></span>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-8">
-            <div class="icheck-primary">
-              <input type="checkbox" id="remember">
-              <label for="remember">
-                Remember Me
-              </label>
-            </div>
-          </div>
-          <!-- /.col -->
-          <div class="col-4">
-            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
-          </div>
-          <!-- /.col -->
-        </div>
-      </form>
-
-      <div class="social-auth-links text-center mt-2 mb-3">
       </div>
-      <!-- /.social-auth-links -->
+    </form>
+  </nav>
+  <!-- /.navbar -->
 
-      <p class="mb-1">
-        <a href="forgot-password.html">I forgot my password</a>
-      </p>
-      <p class="mb-0">
-        <a href="register.php" class="text-center">Register a new membership</a>
-      </p>
+  <div id="sidebar"></div>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0">Main Page</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">Dashboard</li>
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
     </div>
-    <!-- /.card-body -->
-  </div>
-  <!-- /.card -->
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <div class="content">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-lg-6">
+              <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">Manage Users</h3>
+                  </div>
+                <div class="card-body">
+                  <p class="card-text">
+                    <a href="url">Manage Lecturers</a>     
+                  </p>
+                  <p class="card-text">
+                    <a href="url">Manage Students</a>
+                  </p>
+                </div>
+              </div>
+            </div>
+            <!-- /.col-md-6 -->
+            <div class="col-lg-6">
+                <div class="card">
+                  <div class="card-header">
+                      <h3 class="card-title">Assignments</h3>
+                    </div>
+                  <div class="card-body">
+                    <p class="card-text">
+                        <a href="url">Lectures</a>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            <!-- /.col-md-6 -->
+          </div>
+          <!-- /.row -->
+          <div class = "row">
+            <div class="col-lg-6">
+              <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">Contact Us</h3>
+                  </div>
+                <div class="card-body">
+                  <p class="card-text">
+                    <a href="url">FAQ</a>     
+                  </p>
+                  <p class="card-text">
+                    <a href="url">Queries</a>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div><!-- /.container-fluid -->
+      </div>
+      <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+    <div class="p-3">
+      <h5>Title</h5>
+      <p>Sidebar content</p>
+    </div>
+  </aside>
+  <!-- /.control-sidebar -->
+
+  <!-- Main Footer -->
+  <footer class="main-footer">
+    <!-- To the right 
+    <div class="float-right d-none d-sm-inline">
+      Anything you want
+    </div> -->
+    <!-- Default to the left
+    <strong>Copyright &copy; 2014-2020 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved. -->
+  </footer>
 </div>
-<!-- /.login-box -->
+<!-- ./wrapper -->
+
+<!-- REQUIRED SCRIPTS -->
 
 <!-- jQuery -->
-<script src="jss/jquery/jquery.min.js"></script>
+<script src="../jss/jquery/jquery.min.js"></script>
 <!-- Bootstrap 4 -->
-<script src="jss/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="../jss/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- AdminLTE App -->
-<script src="jss/dist/js/adminlte.min.js"></script>
+<script src="../jss/dist/js/adminlte.min.js"></script>
+<script> 
+  $(function(){
+    $("#sidebar").load("sidebar.html"); 
+  });
+  </script> 
 </body>
 </html>
