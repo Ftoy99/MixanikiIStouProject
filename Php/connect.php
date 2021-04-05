28e210e75a79de0c535c46171f7a959b3f338b52
MgcBlade <53795841+MgcBlade@users.noreply.github.com>
2021-04-05 14:40:01 +0300

    Update connect.php

--- Php/connect.php
+++ Php/connect.php
@@ -1,6 +1,6 @@
 <?php
-define('DB_SERVER', '82.102.57.157');
-define('DB_USERNAME', 'localhost');
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 define('DB_NAME', 'cei326');
  
