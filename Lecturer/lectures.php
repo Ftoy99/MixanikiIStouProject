59bc53d13d73933ef2ed6ab7f20d7c16d32613a7
NOP <57258174+mplein2@users.noreply.github.com>
2021-04-05 16:30:13 +0300

    Lectures Start

---
+++ Lecturer/lectures.php
@@ -0,0 +1,221 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>SRS - Lecturer</title>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome Icons -->
+  <link rel="stylesheet" href="../css/fontawesome-free/css/all.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="../css/adminlte.min.css">
+</head>
+
+<body class="hold-transition sidebar-mini">
+  <div class="wrapper">
+
+    <!-- Navbar -->
+    <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+      <!-- Left navbar links -->
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+        </li>
+        <li class="nav-item d-none d-sm-inline-block">
+          <a href="#" class="nav-link">Home</a>
+        </li>
+        <li class="nav-item d-none d-sm-inline-block">
+          <a href="#" class="nav-link">Contact</a>
+        </li>
+      </ul>
+
+      <!-- SEARCH FORM -->
+      <form class="form-inline ml-3">
+        <div class="input-group input-group-sm">
+          <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
+          <div class="input-group-append">
+            <button class="btn btn-navbar" type="submit">
+              <i class="fas fa-search"></i>
+            </button>
+          </div>
+        </div>
+      </form>
+    </nav>
+    <!-- /.navbar -->
+
+    <div id="sidebar"></div>
+
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+      <!-- Content Header (Page header) -->
+      <div class="content-header">
+        <div class="container-fluid">
+          <div class="row mb-2">
+            <div class="col-sm-6">
+              <h1 class="m-0">Lectures</h1>
+            </div><!-- /.col -->
+            <div class="col-sm-6">
+              <ol class="breadcrumb float-sm-right">
+                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                <li class="breadcrumb-item active">Dashboard</li>
+              </ol>
+            </div><!-- /.col -->
+          </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+      </div>
+      <!-- /.content-header -->
+
+      <!-- Main content -->
+      <div class="content">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-12">
+              <div class="card">
+                <div class="card-header">
+                  <h3 class="card-title">Lectures</h3>
+
+                  <div class="card-tools">
+                    <div class="input-group input-group-sm" style="width: 150px;">
+                      <input type="text" name="table_search" class="form-control float-right" placeholder="Search">
+
+                      <div class="input-group-append">
+                        <button type="submit" class="btn btn-default">
+                          <i class="fas fa-search"></i>
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <!-- /.card-header -->
+                <div class="card-body table-responsive p-0">
+                  <table class="table table-hover text-nowrap">
+                    <thead>
+                      <tr>
+                        <th>ID</th>
+                        <th>Title</th>
+                        <th>Date</th>
+                        <th>Time Start</th>
+                        <th>Time End</th>
+                        <th>Status</th>
+                        <th>Edit Button Below</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <?php
+                      include_once('../Php/connect.php');
+                      $sql = "SELECT * FROM lectures";
+                      $result = mysqli_query($con, $sql);
+                      if (mysqli_num_rows($result) > 0) {
+                        // output data of each row
+                        while ($row = mysqli_fetch_assoc($result)) {
+                          echo '
+                          <tr>
+                          <td>' . $row["LectureID"] . '</td>
+                          <td>' . $row["Title"] . '</td>
+                          <td>11-7-2014</td>
+                          <td>' . $row["TimeS"] . '</td>
+                          <td>' . $row["TimeE"] . '</td>
+                          <td></td>
+                          <td>EDIT BUTTON HERE</td>
+                        </tr>
+                        ';
+                        }
+                      }
+                      ?>
+                      <!-- <tr>
+                          <td>183</td>
+                          <td>The Sea</td>
+                          <td>11-7-2014</td>
+                          <td>3</td>
+                          <td>29</td>
+                          <td>29</td>
+                          <td>29</td>
+                        </tr>
+                        <tr>
+                          <td>219</td>
+                          <td>Alexander Pierce</td>
+                          <td>11-7-2014</td>
+                          <td><span class="tag tag-warning">Pending</span></td>
+                          <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
+                          <td>29</td>
+                          <td>29</td>
+                        </tr>
+                        <tr>
+                          <td>657</td>
+                          <td>Bob Doe</td>
+                          <td>11-7-2014</td>
+                          <td><span class="tag tag-primary">Approved</span></td>
+                          <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
+                          <td>29</td>
+                          <td>29</td>
+                        </tr>
+                        <tr>
+                          <td>175</td>
+                          <td>Mike Doe</td>
+                          <td>11-7-2014</td>
+                          <td><span class="tag tag-danger">Denied</span></td>
+                          <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
+                          <td>29</td>
+                          <td>29</td>
+                        </tr> -->
+                    </tbody>
+                  </table>
+                </div>
+                <!-- /.card-body -->
+              </div>
+              <!-- /.card -->
+            </div>
+          </div>
+        </div><!-- /.container-fluid -->
+      </div>
+      <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+
+    <!-- Control Sidebar -->
+    <aside class="control-sidebar control-sidebar-dark">
+      <!-- Control sidebar content goes here -->
+      <div class="p-3">
+        <h5>Title</h5>
+        <p>Sidebar content</p>
+      </div>
+    </aside>
+    <!-- /.control-sidebar -->
+
+    <!-- Main Footer -->
+    <footer class="main-footer">
+      <!-- To the right 
+    <div class="float-right d-none d-sm-inline">
+      Anything you want
+    </div> -->
+      <!-- Default to the left
+    <strong>Copyright &copy; 2014-2020 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved. -->
+    </footer>
+  </div>
+  <!-- ./wrapper -->
+
+  <!-- REQUIRED SCRIPTS -->
+
+  <!-- jQuery -->
+  <script src="../jss/jquery/jquery.min.js"></script>
+  <!-- Bootstrap 4 -->
+  <script src="../jss/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <!-- AdminLTE App -->
+  <script src="../jss/dist/js/adminlte.js"></script>
+  <script>
+    $(function() {
+      $("#sidebar").load("sidebar.php");
+    });
+  </script>
+</body>
+
+</html>
--- Php/connect.php
+++ Php/connect.php
@@ -1,11 +1,11 @@
 <?php
-define('DB_SERVER', '82.102.57.157');
-define('DB_USERNAME', 'localhost');
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 define('DB_NAME', 'cei326');
  
 /* Attempt to connect to MySQL database */
-$con = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+$con = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME,3306);
  
 // Check connection
 if($con === false){
