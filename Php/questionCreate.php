9967af9ed94dde0e417cb6f7b2e73fd791b0e48f
NOP <57258174+mplein2@users.noreply.github.com>
2021-04-06 15:25:46 +0300

    fix

--- Php/questionCreate.php
+++ Php/questionCreate.php
@@ -1,16 +1,15 @@
 <?php
 include_once('connect.php');
+session_start();
 
 $title = $_POST["title"];
 $description = $_POST["description"];
 $id = $_POST["id"];
 
-$sql = "INSERT INTO `questions`(`Title`, `Description`, `AskedBy`) VALUES ('$title','$description','$id');";
+$sql = 'INSERT INTO `questions`(`Title`, `Description`, `AskedBy`) VALUES ("'.$title.'","'.$description.'","'.$id.'");';
 
 if (mysqli_query($con,$sql)){
     echo "TRUE";
 }else{
     echo $sql;
 }
-
-//.
