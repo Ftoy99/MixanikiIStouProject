5ed7007a94e748a419dcf811c3ac654e87466165
NOP <57258174+mplein2@users.noreply.github.com>
2021-05-18 18:07:39 +0300

    fix

--- Php/questionCreate.php
+++ Php/questionCreate.php
@@ -8,10 +8,10 @@ $id = $_POST["id"];
 
 $stmt = mysqli_prepare($con, "INSERT INTO questions(Title,Description,AskedBy) VALUES (?,?,?);");
 mysqli_stmt_bind_param($stmt, 'ssi', $title,$description,$id);
-mysqli_stmt_execute($stmt);
+
 $result = mysqli_stmt_get_result($stmt);
 
-if (mysqli_query($con,$sql)){
+if (mysqli_stmt_execute($stmt)){
     echo "TRUE";
 }else{
     echo $sql;
